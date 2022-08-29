91cd43686018567e175e86f347d54c1b582b74f4
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-08-29 15:33:06 -0300

    Find Project By ID

--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/repositories/ProjectRepository.java
@@ -10,8 +10,9 @@ public interface ProjectRepository extends CrudRepository<Project, Long>{
 
 	@Override
 	default Iterable<Project> findAllById(Iterable<Long> ids) {
-		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	Project findByProjectIdentifier(String projectId);
 
 }
--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/services/ProjectService.java
@@ -20,8 +20,18 @@ public class ProjectService {
 			return projectRepository.save(project);
 		}catch (Exception e) {
 			throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists ");
+		}			
+	}
+	
+	public Project findProjectByIdentifier(String projectId){
+		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if(project == null){
+			throw new ProjectIdException("Project ID '"+projectId+"' does not exists");
 		}
-				
+		
+		return project;
 	}
 
 }
