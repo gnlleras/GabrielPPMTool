e7541f1fde7d4d893a77e318c2cc947fbee79d39
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-08-29 16:19:38 -0300

    Delete Project

--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/services/ProjectService.java
@@ -39,6 +39,18 @@ public class ProjectService {
 	public Iterable<Project> findAllProjects(){
 		return projectRepository.findAll();
 	}
+	
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if(project == null) {
+			throw new ProjectIdException("Cannot Project with ID '"+projectId+"'. This does not exists");
+		}
+		
+		projectRepository.delete(project);
+	}
 
 }
  
