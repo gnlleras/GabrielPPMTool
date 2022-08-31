6342d797f80bdff18a8df5ab82676815d11218e1
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-08-31 12:55:15 -0300

    Add bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
-import Dashboard from './components/Dashboard';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard/>
+        <Dashboard />
       </div>
     );
   }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -7,7 +7,7 @@ class Dashboard extends Component {
     return (
       <div>
         <Header />
-        <h1>Welcome to the Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
         <ProjectItem />
       </div>
     );
