733b635f4aabb7abb72728ed4f55a1dead7f3a8e
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-08-31 10:57:32 -0300

    Header

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Header from "./Layout/Header";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
     return (
+      <div>
+        <Header />
         <h1>Welcome to the Dashboard</h1>
-    )
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
