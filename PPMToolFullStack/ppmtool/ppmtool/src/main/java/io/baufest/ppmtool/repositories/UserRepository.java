cce634e23747d35da287fc5829e1dc974eef9cd9
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-09-16 14:52:05 -0300

    JWT pre-work

--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/repositories/UserRepository.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/repositories/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import io.baufest.ppmtool.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
-
+	User findByUsername(String username);
+	User getById(Long id);
 }
--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/security/SecurityConfig.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/security/SecurityConfig.java
@@ -1,12 +1,22 @@
 package io.baufest.ppmtool.security;
 
+import static io.baufest.ppmtool.security.SecurityConstants.H2_URL;
+import static io.baufest.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import io.baufest.ppmtool.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +26,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 		prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
 	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	@Override
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+
+
+
 	@Autowired
 	private JWTAuthenticationEntryPoint unauthorizedHandler;
 	
@@ -41,7 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 					"/**/*.css",
 					"/**/*.js"
 					).permitAll()
-			.antMatchers("/api/users/**").permitAll() //i have a problem here (video 82)
+			.antMatchers(SIGN_UP_URLS).permitAll()
+			.antMatchers(H2_URL).permitAll()//i have a problem here (video 82)
 			.anyRequest().authenticated();
 	}
 	
