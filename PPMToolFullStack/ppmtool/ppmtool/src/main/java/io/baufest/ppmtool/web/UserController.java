2800e4c284c9a428805107a5f6ab035548bbe4ad
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-09-16 13:58:43 -0300

    User registration part3

--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/web/UserController.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/web/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.baufest.ppmtool.domain.User;
 import io.baufest.ppmtool.services.MapValidationErrorService;
 import io.baufest.ppmtool.services.UserService;
+import io.baufest.ppmtool.validator.UserValidator;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,8 +25,14 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserValidator userValidator;
+	
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
+		
+		userValidator.validate(user, result);
+		
 		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
 		
 		if(errorMap != null)return errorMap;
