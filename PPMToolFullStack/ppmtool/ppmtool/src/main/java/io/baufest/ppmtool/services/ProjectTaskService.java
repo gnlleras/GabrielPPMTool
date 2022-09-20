596ba6c45d81d592f6c7319b7be72207120af783
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-09-20 15:31:08 -0300

    Update and Delete Project Task

--- PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/ppmtool/src/main/java/io/baufest/ppmtool/services/ProjectTaskService.java
@@ -66,13 +66,10 @@ public class ProjectTaskService {
 	}
 	
 	
-	public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id){
+	public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id, String username){
 		
 		//Existing backlog
-		Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
-		if(backlog == null) {
-			throw new ProjectNotFoundException("Project whit ID: '"+backlog_id+"' does not exist");
-		}
+		projectService.findProjectByIdentifier(backlog_id, username);
 		
 		//Existing task
 		ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
@@ -88,16 +85,16 @@ public class ProjectTaskService {
 		return projectTask;
 	}
 	
-	public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id) {
-		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+	public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id, String username) {
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id, username);
 		
 		projectTask = updatedTask;
 		
 		return projectTaskRepository.save(projectTask);
 	}
 	
-	public void deletePTByProjectSequence(String backlog_id, String pt_id) {
-		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+	public void deletePTByProjectSequence(String backlog_id, String pt_id, String username) {
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id, username);
 		
 		projectTaskRepository.delete(projectTask);
 	}
@@ -106,4 +103,5 @@ public class ProjectTaskService {
 	//find existing project task
 	//replace it with update task
 	//save update
+	 
 }
