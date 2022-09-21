e8703c90195eb18f0c0a6bb4a068d2108222b470
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-09-21 17:34:41 -0300

    Handle routing for expired token

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -17,6 +17,7 @@ import Login from "./components/UserManagement/Login";
 import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/securityActions";
 
 const jwtToken = localStorage.jwtToken;
 
@@ -29,6 +30,10 @@ if (jwtToken) {
   });
 
   const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    store.dispatch(logout());
+    window.location.href = "/";
+  }
 }
 
 //Esta clase es la que se renderiza
