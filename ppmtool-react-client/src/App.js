5518c0410f56f59035a6d5d6898abfef25b4ee75
Gabriel Nicolas  Lleras <glleras@baufest.com>
2022-09-22 14:42:30 -0300

    Secure Routes

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux"; // Define la store que usamos en la aplicacion. Union react-redux
@@ -18,6 +18,7 @@ import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
 import { logout } from "./actions/securityActions";
+import SecuredRoute from "./securityUtils/SecureRoute";
 
 const jwtToken = localStorage.jwtToken;
 
@@ -50,20 +51,26 @@ class App extends Component {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
 
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/addProject" component={AddProject} />
-            <Route exact path="/updateProject/:id" component={UpdateProject} />
-            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-            <Route
-              exact
-              path="/addProjectTask/:id"
-              component={AddProjectTask}
-            />
-            <Route
-              exact
-              path="/updateProjectTask/:backlog_id/:pt_id"
-              component={UpdateProjectTask}
-            />
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/addProject" component={AddProject} />
+              <Route
+                exact
+                path="/updateProject/:id"
+                component={UpdateProject}
+              />
+              <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+              <Route
+                exact
+                path="/addProjectTask/:id"
+                component={AddProjectTask}
+              />
+              <Route
+                exact
+                path="/updateProjectTask/:backlog_id/:pt_id"
+                component={UpdateProjectTask}
+              />
+            </Switch>
           </div>
         </Router>
       </Provider>
